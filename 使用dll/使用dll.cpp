9436862b2bfd1cac745ded688b73da5e9ad8f73c
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2024-01-05 05:06:47 +0800

    Add files via upload

---
+++ 使用dll/使用dll.cpp
@@ -0,0 +1,132 @@
+﻿#include <Windows.h>
+#include <stdio.h>
+#include <iostream>
+#include <ctype.h>
+
+int main(void)
+{
+	HMODULE hDll = LoadLibraryW(L"演示dll.dll");
+	if (hDll == NULL)
+	{
+		printf("[%lld]dll load fail,errcode:%d\n", GetTickCount64(), GetLastError());
+		(void)getchar();
+		return -1;
+	}
+
+	printf("[%lld]dll loaded\n", GetTickCount64());
+
+	typedef unsigned int(*WriteData)(const unsigned char *data, unsigned int size);
+	typedef int(*ReadData)(unsigned char *data, unsigned int size);
+	typedef void(*ClearData)(void);
+	typedef unsigned long long (*ReferenceCount)(void);
+	typedef unsigned int (*MaxDataSize)(void);
+	typedef unsigned int (*ZeroData)(unsigned int size);
+	typedef unsigned int (*FillData)(int val, unsigned int size);
+	typedef unsigned int(*SizeData)(void);
+
+	WriteData pWriteData = (WriteData)GetProcAddress(hDll, "WriteData");
+	ReadData pReadData = (ReadData )GetProcAddress(hDll, "ReadData");
+	ClearData pClearData = (ClearData)GetProcAddress(hDll, "ClearData");
+	ReferenceCount pReferenceCount = (ReferenceCount)GetProcAddress(hDll, "ReferenceCount");
+	MaxDataSize pMaxDataSize = (MaxDataSize)GetProcAddress(hDll, "MaxDataSize");
+	ZeroData pZeroData = (ZeroData)GetProcAddress(hDll, "ZeroData");
+	FillData pFillData = (FillData)GetProcAddress(hDll, "FillData");
+	SizeData pSizeData = (SizeData)GetProcAddress(hDll, "SizeData");
+
+	printf("[%lld]proc loaded\n", GetTickCount64());
+
+#define LINE_SIZE 1025
+	unsigned int ret;
+	unsigned char sLine[LINE_SIZE];
+	int c;//使用int接返回值防止EOF截断错误
+
+	while ((c = tolower(getchar())) != EOF)
+	{
+		switch (c)
+		{
+		case 'r'://read
+			{
+				unsigned int length = 0;
+				(void)scanf("%d", &length);
+				if (length > LINE_SIZE)
+				{
+					length = LINE_SIZE;
+				}
+				ret = pReadData(sLine, length);
+				printf("[%lld]read size:%d,data:%.*s\n", GetTickCount64(), ret, ret, sLine);//截断输出，不以0字符结尾
+			}
+			break;
+		case 'w'://write
+			{
+				std::streamsize length = std::cin.get((char *)sLine, LINE_SIZE, '\n').gcount();
+				ret = pWriteData(sLine, (unsigned int)length);//写入数据，不写入0字符
+				printf("[%lld]write size:%d\n", GetTickCount64(), ret);
+			}
+			break;
+		case 's'://size
+			{
+				while (getchar() != '\n') continue;
+				ret = pSizeData();
+				printf("[%lld]data size:%d\n", GetTickCount64(), ret);
+			}
+			break;
+		case 'c'://clear
+			{
+				while (getchar() != '\n') continue;
+				pClearData();
+				printf("[%lld]data clear\n", GetTickCount64());
+			}
+			break;
+		case 'm'://max size
+			{
+				while (getchar() != '\n') continue;
+				ret = pMaxDataSize();
+				printf("[%lld]max data size:%d\n", GetTickCount64(), ret);
+			}
+			break;
+		case 'f'://ref count
+			{
+				while (getchar() != '\n') continue;
+				unsigned long long ret = pReferenceCount();//作用域覆盖重命名
+				printf("[%lld]reference count:%lld\n", GetTickCount64(), ret);
+			}
+			break;
+		case 'q'://quit
+			{
+				FreeLibrary(hDll);
+				printf("[%lld]quit\n", GetTickCount64());
+				return 0;
+			}
+			break;
+		default://其它指令处理
+			{
+				while (c != '\n')//防止当前c就是\n的情况
+				{
+					c = getchar();//清理整行
+				}
+				printf("[%lld]unknow command\n", GetTickCount64());
+			}
+			break;
+		}
+	}
+
+
+	//ret = pWriteData((const unsigned char *)"Hello World", sizeof("Hello World"));
+	//printf("write size:%d\n", ret);
+	//
+	//ret = pSizeData();
+	//printf("data size:%d\n", ret);
+	//
+	//unsigned char cRead[32];
+	//ret = pReadData(cRead, sizeof(cRead));
+	//printf("read size:%d,read data:%.32s\n", ret, cRead);
+	//
+	//pClearData();
+	//printf("clear\n");
+	//
+	//ret = pSizeData();
+	//printf("data size:%d\n", ret);
+
+	FreeLibrary(hDll);
+	return 0;
+}
---
+++ 演示dll/dllmain.cpp
@@ -0,0 +1,128 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "pch.h"
+#include <string.h>
+#include <Windows.h>
+
+#define DATA_SIZE (1024*1024*4)//4MB
+
+#pragma data_seg("Shared")//命名共享数据段
+CRITICAL_SECTION g_cs = {};//同步用关键段
+unsigned long long g_refcount = {};//dll加载引用计数
+/************************************/
+unsigned char g_data[DATA_SIZE] = {};
+unsigned int g_size = {};
+/************************************/
+#pragma data_seg()
+
+#pragma comment(linker,"/SECTION:Shared,RWS")//可读可写可共享
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)//保证单线程调用
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		if (g_refcount++ == 0)//此次操作无序加锁
+		{
+			InitializeCriticalSection(&g_cs);//初始化关键段
+		}
+		break;
+	case DLL_PROCESS_DETACH:
+		if (--g_refcount == 0)//此次操作无序加锁
+		{
+			DeleteCriticalSection(&g_cs);//删除关键段
+		}
+		break;
+	case DLL_THREAD_ATTACH:
+		break;
+	case DLL_THREAD_DETACH:
+		break;
+	default:
+		break;
+	}
+	return TRUE;
+}
+
+extern "C" __declspec(dllexport) unsigned int WriteData(const unsigned char *data, unsigned int size)
+{
+	if (size > DATA_SIZE - g_size)//比剩余空间大
+	{
+		size = DATA_SIZE - g_size;
+	}
+
+	EnterCriticalSection(&g_cs);
+	memcpy(&g_data[g_size], &data[0], size);
+	g_size += size;
+	LeaveCriticalSection(&g_cs);
+
+	return size;
+}
+
+extern "C" __declspec(dllexport) unsigned int ReadData(unsigned char *data, unsigned int size)
+{
+	if (size > g_size)
+	{
+		size = g_size;
+	}
+
+	EnterCriticalSection(&g_cs);
+	memcpy(&data[0], &g_data[0], size);
+	g_size -= size;
+	memmove(&g_data[0], &g_data[size], g_size);
+	LeaveCriticalSection(&g_cs);
+
+	return size;
+}
+
+extern "C" __declspec(dllexport) void ClearData(void)
+{
+	EnterCriticalSection(&g_cs);
+	g_size = 0;
+	LeaveCriticalSection(&g_cs);
+}
+
+extern "C" __declspec(dllexport) unsigned int FillData(int val, unsigned int size)
+{
+	if (size > DATA_SIZE)
+	{
+		size = DATA_SIZE;
+	}
+
+	EnterCriticalSection(&g_cs);
+	memset(g_data, val, size);
+	g_size = size;
+	LeaveCriticalSection(&g_cs);
+
+	return size;
+}
+
+extern "C" __declspec(dllexport) unsigned int ZeroData(unsigned int size)
+{
+	EnterCriticalSection(&g_cs);
+	unsigned int ret = FillData(0, size);
+	LeaveCriticalSection(&g_cs);
+
+	return ret;
+}
+
+extern "C" __declspec(dllexport) unsigned int SizeData(void)
+{
+	EnterCriticalSection(&g_cs);
+	unsigned int ret = g_size;
+	LeaveCriticalSection(&g_cs);
+	
+	return ret;
+}
+
+extern "C" __declspec(dllexport) unsigned int MaxDataSize(void)
+{
+	return DATA_SIZE;
+}
+
+extern "C" __declspec(dllexport) unsigned long long ReferenceCount(void)
+{
+	EnterCriticalSection(&g_cs);
+	unsigned long long ret = g_refcount;//可能多线程调用
+	LeaveCriticalSection(&g_cs);
+
+	return ret;
+}
